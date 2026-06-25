3b10aba25c401be96d910254d0bffb300020edfa
a-alnas <abdullahalnasrallah123@gmail.com>
2026-06-26 02:35:35 +0300

    changed header to test time machine

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 function App() {
   return (
       <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
-        <h1>Welcome to Sharqia Study Hub</h1>
+        <h1>Testing time machine</h1>
         <p>Your full-stack learning platform is officially ready for design!</p>
       </div>
   )
