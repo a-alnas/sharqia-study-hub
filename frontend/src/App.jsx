a19cdd4c51b95cb6d3e8b648316b7a7ba675df12
a-alnas <abdullahalnasrallah123@gmail.com>
2026-06-26 02:17:18 +0300

    Initial commit: Set up Express backend and React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+      <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+        <h1>Welcome to Sharqia Study Hub</h1>
+        <p>Your full-stack learning platform is officially ready for design!</p>
+      </div>
+  )
+}
+
+export default App
